d8865ffa211c7605f196b9d3a10b99542b72c6fe
puja-greycube <puja@greycube.in>
2023-06-28 18:11:51 +0530

    feat:day 3 task

--- airplane_mode/flight_ticket_system/doctype/airline/airline.js
+++ airplane_mode/flight_ticket_system/doctype/airline/airline.js
@@ -2,7 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Airline', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (!frm.doc.website) {
+			//frappe.msgprint("No website added");
+		}
+		else{
+			frm.add_web_link('https://www.spicejet.com/','Visit Website')
+		}
+	}
 });
--- airplane_mode/flight_ticket_system/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/flight_ticket_system/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,17 @@
-// Copyright (c) 2023, Vijay and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Airplane Ticket', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button('Assign Seat', function() {
+			frappe.prompt([
+				{
+					fieldname: 'seat_number',
+					label: 'Seat Number',
+					fieldtype: 'Data',
+					reqd: 1
+				}
+			], function(values){
+				frm.set_value('seat', values.seat_number);
+				frm.refresh_field('seat');
+			}, 'Seat Number','Assign');
+		}, 'Actions');
+	}
 });
